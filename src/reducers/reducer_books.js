93a92c202325317d98d9de9099b909d0c3a4478b
wendyhuanglj <lijuan.huang@csaa.com>
2016-06-02 18:51:14 -0700

    adding frame of book redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: ' The Glass Castle ' },
+    { title: ' A Million Little Pieces ' },
+    { title: ' Go Ask Alice ' },
+    { title: ' Augusta Gone ' }
+  ]
+}
